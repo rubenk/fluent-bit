5fdd064c5b2f5d70e46fa2b3e9664cb52a8ce17d
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-07-22 16:14:09 +0200

    Implement init callback

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -19,7 +19,42 @@
 
 #include <fluent-bit/flb_input_plugin.h>
 
-static int in_maces_init(struct flb_input_instance *ins, struct flb_config *config, void *data) {
+#include <EndpointSecurity/EndpointSecurity.h>
+
+static es_handler_block_t handler =  ^(es_client_t *c, const es_message_t *msg ) {
+};
+
+static int in_maces_init(struct flb_input_instance *in, struct flb_config *config, void *data) {
+  es_client_t *client;
+  es_new_client_result_t res = es_new_client(&client, handler);
+  if (res != ES_NEW_CLIENT_RESULT_SUCCESS) {
+    switch(res) {
+      case ES_NEW_CLIENT_RESULT_ERR_NOT_ENTITLED:
+        flb_plg_error(in, "Application requires 'com.apple.developer.endpoint-security.client' entitlement");
+        break;
+      case ES_NEW_CLIENT_RESULT_ERR_NOT_PERMITTED:
+        flb_plg_error(in, "Application lacks 'Transparency, Consent, and Control (TCC)' approval");
+        break;
+      case ES_NEW_CLIENT_RESULT_ERR_NOT_PRIVILEGED:
+        flb_plg_error(in, "Application needs to run as root");
+        break;
+      default:
+        flb_plg_error(in, "Unknown error");
+        break;
+    }
+    return 1;
+  }
+
+  flb_plg_info(in, "Endpoint Security Client initialized successfully a");
+  es_event_type_t events[] = {ES_EVENT_TYPE_NOTIFY_EXEC};
+  es_return_t subscribed = es_subscribe(client, events, sizeof events / sizeof *events);
+  if(subscribed != ES_RETURN_SUCCESS) {
+    flb_plg_error(in, "Error subscribing to events");
+    return 1;
+  } else {
+    flb_plg_info(in, "Subscribed to events");
+  }
+
   return 0;
 }
 
