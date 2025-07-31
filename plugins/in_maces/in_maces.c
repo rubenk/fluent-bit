e2638d8a1b16bfec90905c46e2bb9b9e3e3469f0
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-07-31 22:33:48 +0200

    Implement encoding of uipc_connect syscall

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -427,6 +427,26 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                     FLB_LOG_EVENT_UINT32_VALUE(event.uipc_bind.mode));
                 flb_log_event_encoder_body_commit_map(ctx->encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_UIPC_CONNECT:
+                flb_log_event_encoder_body_begin_map(ctx->encoder);
+                flb_log_event_encoder_append_body_cstring(
+                    ctx->encoder,
+                    "file");
+                encode_es_file_t(ctx->encoder, event.uipc_connect.file);
+                flb_log_event_encoder_append_body_values(
+                    ctx->encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("domain"),
+                    FLB_LOG_EVENT_INT32_VALUE(event.uipc_connect.domain));
+                flb_log_event_encoder_append_body_values(
+                    ctx->encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("type"),
+                    FLB_LOG_EVENT_INT32_VALUE(event.uipc_connect.type));
+                flb_log_event_encoder_append_body_values(
+                    ctx->encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("protocol"),
+                    FLB_LOG_EVENT_INT32_VALUE(event.uipc_connect.protocol));
+                flb_log_event_encoder_body_commit_map(ctx->encoder);
+                break;
             default:
                 flb_log_event_encoder_append_body_null(ctx->encoder);
                 break;
