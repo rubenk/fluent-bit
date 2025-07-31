f6f8936de9d7be224c817c72a0eb38fb6a121034
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-07-31 21:29:18 +0200

    Implement encoding of the mprotect syscall

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -391,6 +391,22 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                 encode_es_file_t(ctx->encoder, event.mmap.source);
                 flb_log_event_encoder_body_commit_map(ctx->encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_MPROTECT:
+                flb_log_event_encoder_body_begin_map(ctx->encoder);
+                flb_log_event_encoder_append_body_values(
+                    ctx->encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("protection"),
+                    FLB_LOG_EVENT_INT32_VALUE(event.mprotect.protection));
+                flb_log_event_encoder_append_body_values(
+                    ctx->encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("address"),
+                    FLB_LOG_EVENT_UINT64_VALUE(event.mprotect.address));
+                flb_log_event_encoder_append_body_values(
+                    ctx->encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("size"),
+                    FLB_LOG_EVENT_UINT64_VALUE(event.mprotect.size));
+                flb_log_event_encoder_body_commit_map(ctx->encoder);
+                break;
             default:
                 flb_log_event_encoder_append_body_null(ctx->encoder);
                 break;
