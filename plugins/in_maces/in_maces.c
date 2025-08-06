58c68531a7ee2bbb2d0dba3563e99362e42ff56e
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-08-06 22:25:56 +0200

    Implement encoding of deleteextattr syscall

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -574,6 +574,18 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                     FLB_LOG_EVENT_STRING_VALUE(event.getextattr.extattr.data, event.getextattr.extattr.length));
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_DELETEEXTATTR:
+                flb_log_event_encoder_body_begin_map(encoder);
+                flb_log_event_encoder_append_body_cstring(
+                    encoder,
+                    "target");
+                encode_es_file_t(encoder, event.deleteextattr.target);
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("extattr"),
+                    FLB_LOG_EVENT_STRING_VALUE(event.deleteextattr.extattr.data, event.deleteextattr.extattr.length));
+                flb_log_event_encoder_body_commit_map(encoder);
+                break;
             default:
                 flb_log_event_encoder_append_body_null(encoder);
                 break;
