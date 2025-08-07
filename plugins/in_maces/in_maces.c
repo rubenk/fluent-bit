1a20b290881813bef477e80af67ed33a7f62d135
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-08-07 20:46:40 +0200

    Implement encoding of chroot syscall

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -634,6 +634,14 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                 encode_es_file_t(encoder, event.stat.target);
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_CHROOT:
+                flb_log_event_encoder_body_begin_map(encoder);
+                flb_log_event_encoder_append_body_cstring(
+                    encoder,
+                    "target");
+                encode_es_file_t(encoder, event.chroot.target);
+                flb_log_event_encoder_body_commit_map(encoder);
+                break;
             default:
                 flb_log_event_encoder_append_body_null(encoder);
                 break;
