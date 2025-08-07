81c9702769130685e42b27c49c59b9bcc6725780
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-08-07 20:55:18 +0200

    Implement encoding of listextattr syscall

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -550,6 +550,13 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                 }
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_LISTEXTATTR:
+                flb_log_event_encoder_body_begin_map(encoder);
+                flb_log_event_encoder_append_body_cstring(
+                    encoder,
+                    "target");
+                encode_es_file_t(encoder, event.listextattr.target);
+                break;
             case ES_EVENT_TYPE_NOTIFY_SETEXTATTR:
                 flb_log_event_encoder_body_begin_map(encoder);
                 flb_log_event_encoder_append_body_cstring(
