b101511901baa23b8fc79a66a9f2bf5ced33bbb5
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-08-11 22:00:51 +0200

    Implement encoding of utimes syscall

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -1095,6 +1095,18 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                 encode_es_file_t(encoder, event.searchfs.target);
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_UTIMES:
+                flb_log_event_encoder_body_begin_map(encoder);
+                flb_log_event_encoder_append_body_cstring(encoder, "atime");
+                encode_timespec(encoder, &event.utimes.atime);
+                flb_log_event_encoder_append_body_cstring(encoder, "mtime");
+                encode_timespec(encoder, &event.utimes.mtime);
+                flb_log_event_encoder_append_body_cstring(
+                    encoder,
+                    "target");
+                encode_es_file_t(encoder, event.utimes.target);
+                flb_log_event_encoder_body_commit_map(encoder);
+                break;
             default:
                 flb_log_event_encoder_append_body_null(encoder);
                 break;
