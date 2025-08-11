f73f39a29d4bb1da2bec37e7a3c81260742fe112
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-08-11 11:21:51 +0200

    Implement encoding of es_event_clone_t

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -681,6 +681,22 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                     FLB_LOG_EVENT_STRING_VALUE(event.lookup.relative_target.data, event.lookup.relative_target.length));
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_CLONE:
+                flb_log_event_encoder_body_begin_map(encoder);
+                flb_log_event_encoder_append_body_cstring(
+                    encoder,
+                    "source");
+                encode_es_file_t(encoder, event.clone.source);
+                flb_log_event_encoder_append_body_cstring(
+                    encoder,
+                    "target_dir");
+                encode_es_file_t(encoder, event.clone.target_dir);
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("target_name"),
+                    FLB_LOG_EVENT_STRING_VALUE(event.clone.target_name.data, event.clone.target_name.length));
+                flb_log_event_encoder_body_commit_map(encoder);
+                break;
             default:
                 flb_log_event_encoder_append_body_null(encoder);
                 break;
