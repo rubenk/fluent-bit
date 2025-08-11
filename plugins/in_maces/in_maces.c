40abb24f6e1485bf7d51e9ee2192fd04042c1a80
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-08-11 12:20:27 +0200

    Implement encoding of task inspect and name events

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -825,6 +825,34 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                 }
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_GET_TASK_INSPECT:
+                flb_log_event_encoder_body_begin_map(encoder);
+                flb_log_event_encoder_append_body_cstring(
+                    encoder,
+                    "target");
+                encode_es_process_t(encoder, event.get_task_inspect.target);
+                if (msg->version >= 5) {
+                  flb_log_event_encoder_append_body_values(
+                      encoder,
+                      FLB_LOG_EVENT_CSTRING_VALUE("type"),
+                      FLB_LOG_EVENT_INT32_VALUE(event.get_task_inspect.type));
+                }
+                flb_log_event_encoder_body_commit_map(encoder);
+                break;
+            case ES_EVENT_TYPE_NOTIFY_GET_TASK_NAME:
+                flb_log_event_encoder_body_begin_map(encoder);
+                flb_log_event_encoder_append_body_cstring(
+                    encoder,
+                    "target");
+                encode_es_process_t(encoder, event.get_task_name.target);
+                if (msg->version >= 5) {
+                  flb_log_event_encoder_append_body_values(
+                      encoder,
+                      FLB_LOG_EVENT_CSTRING_VALUE("type"),
+                      FLB_LOG_EVENT_INT32_VALUE(event.get_task_name.type));
+                }
+                flb_log_event_encoder_body_commit_map(encoder);
+                break;
             default:
                 flb_log_event_encoder_append_body_null(encoder);
                 break;
