a5a8ef34c9a48fd4d3950c09fe939a8792b3645a
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-08-11 11:55:04 +0200

    Implement encoding of kextload event

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -753,6 +753,14 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                 encode_es_file_t(encoder, event.fsgetpath.target);
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_KEXTLOAD:
+                flb_log_event_encoder_body_begin_map(encoder);
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("identifier"),
+                    FLB_LOG_EVENT_STRING_VALUE(event.kextload.identifier.data, event.kextload.identifier.length));
+                flb_log_event_encoder_body_commit_map(encoder);
+                break;
             default:
                 flb_log_event_encoder_append_body_null(encoder);
                 break;
