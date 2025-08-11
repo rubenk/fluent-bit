8c4b4e656140013a91a0cf279b81867211fe21ea
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-08-11 12:17:51 +0200

    Implement encoding of es_event_get_task_read_t

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -811,6 +811,20 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                 }
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_GET_TASK_READ:
+                flb_log_event_encoder_body_begin_map(encoder);
+                flb_log_event_encoder_append_body_cstring(
+                    encoder,
+                    "target");
+                encode_es_process_t(encoder, event.get_task_read.target);
+                if (msg->version >= 5) {
+                  flb_log_event_encoder_append_body_values(
+                      encoder,
+                      FLB_LOG_EVENT_CSTRING_VALUE("type"),
+                      FLB_LOG_EVENT_INT32_VALUE(event.get_task_read.type));
+                }
+                flb_log_event_encoder_body_commit_map(encoder);
+                break;
             default:
                 flb_log_event_encoder_append_body_null(encoder);
                 break;
