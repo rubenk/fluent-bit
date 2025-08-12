d80c363bf38bd567029043e792a84d0c61a067fd
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-08-12 22:16:30 +0200

    Implement encoding of the setegid syscall

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -1467,6 +1467,14 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                     FLB_LOG_EVENT_UINT32_VALUE(event.seteuid.euid));
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_SETEGID:
+                flb_log_event_encoder_body_begin_map(encoder);
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("egid"),
+                    FLB_LOG_EVENT_UINT32_VALUE(event.setegid.egid));
+                flb_log_event_encoder_body_commit_map(encoder);
+                break;
             default:
                 flb_log_event_encoder_append_body_null(encoder);
                 break;
