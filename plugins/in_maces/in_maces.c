405e57fb831b20f2b509ab71f0fa3fc68a2fa1a9
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-08-12 10:33:22 +0200

    Implement encoding of screensharing detachment

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -1203,6 +1203,30 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                     FLB_LOG_EVENT_UINT32_VALUE(event.screensharing_attach->graphical_session_id));
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_SCREENSHARING_DETACH:
+                flb_log_event_encoder_body_begin_map(encoder);
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("source_address_type"),
+                    FLB_LOG_EVENT_INT32_VALUE(event.screensharing_detach->source_address_type));
+                if (event.screensharing_detach->source_address.length > 0) {
+                  flb_log_event_encoder_append_body_values(
+                      encoder,
+                      FLB_LOG_EVENT_CSTRING_VALUE("source_address"),
+                      FLB_LOG_EVENT_STRING_VALUE(event.screensharing_detach->source_address.data, event.screensharing_detach->source_address.length));
+                }
+                if (event.screensharing_detach->viewer_appleid.length > 0) {
+                  flb_log_event_encoder_append_body_values(
+                      encoder,
+                      FLB_LOG_EVENT_CSTRING_VALUE("viewer_appleid"),
+                      FLB_LOG_EVENT_STRING_VALUE(event.screensharing_detach->viewer_appleid.data, event.screensharing_detach->viewer_appleid.length));
+                }
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("graphical_session_id"),
+                    FLB_LOG_EVENT_UINT32_VALUE(event.screensharing_detach->graphical_session_id));
+                flb_log_event_encoder_body_commit_map(encoder);
+                break;
             default:
                 flb_log_event_encoder_append_body_null(encoder);
                 break;
