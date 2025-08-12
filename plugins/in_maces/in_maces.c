ae3c731f3fd6c53b4c361fc30f041a8daca3cb2d
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-08-12 09:52:12 +0200

    Implement encoding session unlock event

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -1143,6 +1143,18 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                     FLB_LOG_EVENT_UINT32_VALUE(event.lw_session_lock->graphical_session_id));
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_LW_SESSION_UNLOCK:
+                flb_log_event_encoder_body_begin_map(encoder);
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("username"),
+                    FLB_LOG_EVENT_STRING_VALUE(event.lw_session_unlock->username.data, event.lw_session_unlock->username.length));
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("graphical_session_id"),
+                    FLB_LOG_EVENT_UINT32_VALUE(event.lw_session_unlock->graphical_session_id));
+                flb_log_event_encoder_body_commit_map(encoder);
+                break;
             default:
                 flb_log_event_encoder_append_body_null(encoder);
                 break;
