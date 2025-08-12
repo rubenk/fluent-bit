180212f54f6245013d207f5c6c168a764c101bc4
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-08-12 22:18:52 +0200

    Implement encoding of the setreuid syscall

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -1475,6 +1475,18 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                     FLB_LOG_EVENT_UINT32_VALUE(event.setegid.egid));
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_SETREUID:
+                flb_log_event_encoder_body_begin_map(encoder);
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("ruid"),
+                    FLB_LOG_EVENT_UINT32_VALUE(event.setreuid.ruid));
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("euid"),
+                    FLB_LOG_EVENT_UINT32_VALUE(event.setreuid.euid));
+                flb_log_event_encoder_body_commit_map(encoder);
+                break;
             default:
                 flb_log_event_encoder_append_body_null(encoder);
                 break;
