d354cea5e14ebfe4afa7aa2cb7d57e99ccd305ef
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-08-12 09:46:24 +0200

    Implement encoding of login window logout event

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -1119,6 +1119,18 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                     FLB_LOG_EVENT_UINT32_VALUE(event.lw_session_login->graphical_session_id));
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_LW_SESSION_LOGOUT:
+                flb_log_event_encoder_body_begin_map(encoder);
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("username"),
+                    FLB_LOG_EVENT_STRING_VALUE(event.lw_session_logout->username.data, event.lw_session_logout->username.length));
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("graphical_session_id"),
+                    FLB_LOG_EVENT_UINT32_VALUE(event.lw_session_logout->graphical_session_id));
+                flb_log_event_encoder_body_commit_map(encoder);
+                break;
             default:
                 flb_log_event_encoder_append_body_null(encoder);
                 break;
