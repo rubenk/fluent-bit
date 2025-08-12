50601a040431eb07450b8f97d6cdd04bb38781e7
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-08-12 10:16:42 +0200

    Implement encoding of screensharing attach event
    This event triggers when someone logs in with Apple Remote Desktop

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -1155,6 +1155,54 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                     FLB_LOG_EVENT_UINT32_VALUE(event.lw_session_unlock->graphical_session_id));
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_SCREENSHARING_ATTACH:
+                flb_log_event_encoder_body_begin_map(encoder);
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("success"),
+                    FLB_LOG_EVENT_BOOLEAN_VALUE(event.screensharing_attach->success));
+                  flb_log_event_encoder_append_body_values(
+                      encoder,
+                      FLB_LOG_EVENT_CSTRING_VALUE("source_address_type"),
+                      FLB_LOG_EVENT_INT32_VALUE(event.screensharing_attach->source_address_type));
+                if (event.screensharing_attach->source_address.length > 0) {
+                  flb_log_event_encoder_append_body_values(
+                      encoder,
+                      FLB_LOG_EVENT_CSTRING_VALUE("source_address"),
+                      FLB_LOG_EVENT_STRING_VALUE(event.screensharing_attach->source_address.data, event.screensharing_attach->source_address.length));
+                }
+                if (event.screensharing_attach->viewer_appleid.length > 0) {
+                  flb_log_event_encoder_append_body_values(
+                      encoder,
+                      FLB_LOG_EVENT_CSTRING_VALUE("viewer_appleid"),
+                      FLB_LOG_EVENT_STRING_VALUE(event.screensharing_attach->viewer_appleid.data, event.screensharing_attach->viewer_appleid.length));
+                }
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("authentication_type"),
+                    FLB_LOG_EVENT_STRING_VALUE(event.screensharing_attach->authentication_type.data, event.screensharing_attach->authentication_type.length));
+                if (event.screensharing_attach->authentication_username.length > 0) {
+                  flb_log_event_encoder_append_body_values(
+                      encoder,
+                      FLB_LOG_EVENT_CSTRING_VALUE("authentication_username"),
+                      FLB_LOG_EVENT_STRING_VALUE(event.screensharing_attach->authentication_username.data, event.screensharing_attach->authentication_username.length));
+                }
+                if (event.screensharing_attach->session_username.length > 0) {
+                  flb_log_event_encoder_append_body_values(
+                      encoder,
+                      FLB_LOG_EVENT_CSTRING_VALUE("session_username"),
+                      FLB_LOG_EVENT_STRING_VALUE(event.screensharing_attach->session_username.data, event.screensharing_attach->session_username.length));
+                }
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("existing_session"),
+                    FLB_LOG_EVENT_BOOLEAN_VALUE(event.screensharing_attach->existing_session));
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("graphical_session_id"),
+                    FLB_LOG_EVENT_UINT32_VALUE(event.screensharing_attach->graphical_session_id));
+                flb_log_event_encoder_body_commit_map(encoder);
+                break;
             default:
                 flb_log_event_encoder_append_body_null(encoder);
                 break;
