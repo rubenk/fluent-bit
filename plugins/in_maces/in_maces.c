83813887bdf6557e0678f0d6422a2e7e768de584
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-08-12 09:44:15 +0200

    Implement encoding of login window login event

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -1107,6 +1107,18 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                 encode_es_file_t(encoder, event.utimes.target);
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_LW_SESSION_LOGIN:
+                flb_log_event_encoder_body_begin_map(encoder);
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("username"),
+                    FLB_LOG_EVENT_STRING_VALUE(event.lw_session_login->username.data, event.lw_session_login->username.length));
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("graphical_session_id"),
+                    FLB_LOG_EVENT_UINT32_VALUE(event.lw_session_login->graphical_session_id));
+                flb_log_event_encoder_body_commit_map(encoder);
+                break;
             default:
                 flb_log_event_encoder_append_body_null(encoder);
                 break;
