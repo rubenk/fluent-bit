e00b20a3f23adb559ca8bb63ee64a9cb60791125
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-08-12 10:45:13 +0200

    Implement encoding of openssh logouts

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -1261,6 +1261,26 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                 }
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_OPENSSH_LOGOUT:
+                flb_log_event_encoder_body_begin_map(encoder);
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("source_address_type"),
+                    FLB_LOG_EVENT_INT32_VALUE(event.openssh_logout->source_address_type));
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("source_address"),
+                    FLB_LOG_EVENT_STRING_VALUE(event.openssh_logout->source_address.data, event.openssh_logout->source_address.length));
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("username"),
+                    FLB_LOG_EVENT_STRING_VALUE(event.openssh_logout->username.data, event.openssh_logout->username.length));
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("uid"),
+                    FLB_LOG_EVENT_UINT32_VALUE(event.openssh_logout->uid));
+                flb_log_event_encoder_body_commit_map(encoder);
+                break;
             default:
                 flb_log_event_encoder_append_body_null(encoder);
                 break;
