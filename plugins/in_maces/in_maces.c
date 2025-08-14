801b202d6ee913a1c581a5270f3df282458e95bf
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-08-14 22:02:03 +0200

    Implement encoding of od_attribute_value_remove event
    This can be triggered by running `dscl . -rm /Users/www Comment "This is a comment"`

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -1924,6 +1924,51 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                 }
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_OD_ATTRIBUTE_VALUE_REMOVE:
+                es_event_od_attribute_value_remove_t *od_attribute_value_remove = event.od_attribute_value_remove;
+                flb_log_event_encoder_body_begin_map(encoder);
+                flb_log_event_encoder_append_body_cstring(
+                    encoder,
+                    "instigator");
+                if (od_attribute_value_remove->instigator) {
+                    encode_es_process_t(encoder, od_attribute_value_remove->instigator);
+                } else {
+                    flb_log_event_encoder_append_body_null(encoder);
+                }
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("error_code"),
+                    FLB_LOG_EVENT_INT32_VALUE(od_attribute_value_remove->error_code));
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("record_name"),
+                    FLB_LOG_EVENT_STRING_VALUE(od_attribute_value_remove->record_name.data, od_attribute_value_remove->record_name.length));
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("attribute_name"),
+                    FLB_LOG_EVENT_STRING_VALUE(od_attribute_value_remove->attribute_name.data, od_attribute_value_remove->attribute_name.length));
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("attribute_value"),
+                    FLB_LOG_EVENT_STRING_VALUE(od_attribute_value_remove->attribute_value.data, od_attribute_value_remove->attribute_value.length));
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("node_name"),
+                    FLB_LOG_EVENT_STRING_VALUE(od_attribute_value_remove->node_name.data, od_attribute_value_remove->node_name.length));
+                if(od_attribute_value_remove->db_path.length > 0) {
+                    flb_log_event_encoder_append_body_values(
+                        encoder,
+                        FLB_LOG_EVENT_CSTRING_VALUE("db_path"),
+                        FLB_LOG_EVENT_STRING_VALUE(od_attribute_value_remove->db_path.data, od_attribute_value_remove->db_path.length));
+                }
+                if (msg->version >= 8) {
+                  flb_log_event_encoder_append_body_cstring(
+                      encoder,
+                      "instigator_token");
+                  encode_audit_token_t(encoder, &od_attribute_value_remove->instigator_token);
+                }
+                flb_log_event_encoder_body_commit_map(encoder);
+                break;
             default:
                 flb_log_event_encoder_append_body_null(encoder);
                 break;
