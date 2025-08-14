5e195201edbe9a054d3e4d066fe4706dcd054b9c
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-08-14 22:21:29 +0200

    Implement encoding od_attribute_set event
    This triggers when enabling a user:
    `dscl . -read /Users/test AuthenticationAuthority | sed 's/AuthenticationAuthority: //;s/DisabledUser//g;s/[; ]*$//' | xargs dscl . -create /Users/test AuthenticationAuthority`

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -1977,6 +1977,65 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                 }
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_OD_ATTRIBUTE_SET:
+                es_event_od_attribute_set_t *od_attribute_set = event.od_attribute_set;
+                flb_log_event_encoder_body_begin_map(encoder);
+                flb_log_event_encoder_append_body_cstring(
+                    encoder,
+                    "instigator");
+                if (od_attribute_set->instigator) {
+                    encode_es_process_t(encoder, od_attribute_set->instigator);
+                } else {
+                    flb_log_event_encoder_append_body_null(encoder);
+                }
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("error_code"),
+                    FLB_LOG_EVENT_INT32_VALUE(od_attribute_set->error_code));
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("record_type"),
+                    FLB_LOG_EVENT_INT32_VALUE(od_attribute_set->record_type));
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("record_name"),
+                    FLB_LOG_EVENT_STRING_VALUE(od_attribute_set->record_name.data, od_attribute_set->record_name.length));
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("attribute_name"),
+                    FLB_LOG_EVENT_STRING_VALUE(od_attribute_set->attribute_name.data, od_attribute_set->attribute_name.length));
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("attribute_value_count"),
+                    FLB_LOG_EVENT_UINT64_VALUE(od_attribute_set->attribute_value_count));
+                flb_log_event_encoder_append_body_cstring(
+                    encoder,
+                    "attribute_values");
+                flb_log_event_encoder_body_begin_array(encoder);
+                for(size_t i = 0; i < od_attribute_set->attribute_value_count; i++) {
+                    flb_log_event_encoder_append_body_string(
+                        encoder,
+                        od_attribute_set->attribute_values[i].data, od_attribute_set->attribute_values[i].length);
+                }
+                flb_log_event_encoder_body_commit_array(encoder);
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("node_name"),
+                    FLB_LOG_EVENT_STRING_VALUE(od_attribute_set->node_name.data, od_attribute_set->node_name.length));
+                if(od_attribute_set->db_path.length > 0) {
+                    flb_log_event_encoder_append_body_values(
+                        encoder,
+                        FLB_LOG_EVENT_CSTRING_VALUE("db_path"),
+                        FLB_LOG_EVENT_STRING_VALUE(od_attribute_set->db_path.data, od_attribute_set->db_path.length));
+                }
+                if (msg->version >= 8) {
+                  flb_log_event_encoder_append_body_cstring(
+                      encoder,
+                      "instigator_token");
+                  encode_audit_token_t(encoder, &od_attribute_set->instigator_token);
+                }
+                flb_log_event_encoder_body_commit_map(encoder);
+                break;
             default:
                 flb_log_event_encoder_append_body_null(encoder);
                 break;
