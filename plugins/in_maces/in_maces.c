fab72a845475a3b5030f5593a5c77d9c40ec1e64
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-08-14 10:35:15 +0200

    Remove one level of indirection

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -1560,12 +1560,13 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
             case ES_EVENT_TYPE_NOTIFY_SUDO:
+                es_event_sudo_t *sudo = event.sudo;
                 flb_log_event_encoder_body_begin_map(encoder);
                 flb_log_event_encoder_append_body_values(
                     encoder,
                     FLB_LOG_EVENT_CSTRING_VALUE("success"),
-                    FLB_LOG_EVENT_BOOLEAN_VALUE(event.sudo->success));
-                if(!event.sudo->success) {
+                    FLB_LOG_EVENT_BOOLEAN_VALUE(sudo->success));
+                if(!sudo->success) {
                     flb_log_event_encoder_append_body_cstring(
                         encoder,
                         "reject_info");
@@ -1573,54 +1574,54 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                     flb_log_event_encoder_append_body_values(
                         encoder,
                         FLB_LOG_EVENT_CSTRING_VALUE("plugin_name"),
-                        FLB_LOG_EVENT_STRING_VALUE(event.sudo->reject_info->plugin_name.data, event.sudo->reject_info->plugin_name.length));
+                        FLB_LOG_EVENT_STRING_VALUE(sudo->reject_info->plugin_name.data, sudo->reject_info->plugin_name.length));
                         flb_log_event_encoder_append_body_values(
                             encoder,
                             FLB_LOG_EVENT_CSTRING_VALUE("plugin_type"),
-                            FLB_LOG_EVENT_INT32_VALUE(event.sudo->reject_info->plugin_type));
+                            FLB_LOG_EVENT_INT32_VALUE(sudo->reject_info->plugin_type));
                         flb_log_event_encoder_append_body_values(
                             encoder,
                             FLB_LOG_EVENT_CSTRING_VALUE("failure_message"),
-                            FLB_LOG_EVENT_STRING_VALUE(event.sudo->reject_info->failure_message.data, event.sudo->reject_info->failure_message.length));
+                            FLB_LOG_EVENT_STRING_VALUE(sudo->reject_info->failure_message.data, sudo->reject_info->failure_message.length));
                     flb_log_event_encoder_body_commit_map(encoder);
                 }
                 flb_log_event_encoder_append_body_values(
                     encoder,
                     FLB_LOG_EVENT_CSTRING_VALUE("has_from_uid"),
-                    FLB_LOG_EVENT_BOOLEAN_VALUE(event.sudo->has_from_uid));
-                if (event.sudo->has_from_uid) {
+                    FLB_LOG_EVENT_BOOLEAN_VALUE(sudo->has_from_uid));
+                if (sudo->has_from_uid) {
                   flb_log_event_encoder_append_body_values(
                       encoder,
                       FLB_LOG_EVENT_CSTRING_VALUE("from_uid"),
-                      FLB_LOG_EVENT_UINT32_VALUE(event.sudo->from_uid.uid));
+                      FLB_LOG_EVENT_UINT32_VALUE(sudo->from_uid.uid));
                 }
-                if (event.sudo->from_username.length > 0) {
+                if (sudo->from_username.length > 0) {
                   flb_log_event_encoder_append_body_values(
                       encoder,
                       FLB_LOG_EVENT_CSTRING_VALUE("from_username"),
-                      FLB_LOG_EVENT_STRING_VALUE(event.sudo->from_username.data, event.sudo->from_username.length));
+                      FLB_LOG_EVENT_STRING_VALUE(sudo->from_username.data, sudo->from_username.length));
                 }
                 flb_log_event_encoder_append_body_values(
                     encoder,
                     FLB_LOG_EVENT_CSTRING_VALUE("has_to_uid"),
-                    FLB_LOG_EVENT_BOOLEAN_VALUE(event.sudo->has_to_uid));
-                if (event.sudo->has_to_uid) {
+                    FLB_LOG_EVENT_BOOLEAN_VALUE(sudo->has_to_uid));
+                if (sudo->has_to_uid) {
                   flb_log_event_encoder_append_body_values(
                       encoder,
                       FLB_LOG_EVENT_CSTRING_VALUE("to_uid"),
-                      FLB_LOG_EVENT_UINT32_VALUE(event.sudo->to_uid.uid));
+                      FLB_LOG_EVENT_UINT32_VALUE(sudo->to_uid.uid));
                 }
-                if (event.sudo->to_username.length > 0) {
+                if (sudo->to_username.length > 0) {
                   flb_log_event_encoder_append_body_values(
                       encoder,
                       FLB_LOG_EVENT_CSTRING_VALUE("to_username"),
-                      FLB_LOG_EVENT_STRING_VALUE(event.sudo->to_username.data, event.sudo->to_username.length));
+                      FLB_LOG_EVENT_STRING_VALUE(sudo->to_username.data, sudo->to_username.length));
                 }
-                if(event.sudo->command.length > 0) {
+                if(sudo->command.length > 0) {
                     flb_log_event_encoder_append_body_values(
                         encoder,
                         FLB_LOG_EVENT_CSTRING_VALUE("command"),
-                        FLB_LOG_EVENT_STRING_VALUE(event.sudo->command.data, event.sudo->command.length));
+                        FLB_LOG_EVENT_STRING_VALUE(sudo->command.data, sudo->command.length));
                 }
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
