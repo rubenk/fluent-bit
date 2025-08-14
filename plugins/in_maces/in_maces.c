49f2ee923633941c6520cc1b752e9b8eb7d8d140
Ruben Kerkhof <ruben.kerkhof@huntandhackett.com>
2025-08-14 11:25:44 +0200

    Implement encoding the group creation event

--- plugins/in_maces/in_maces.c
+++ plugins/in_maces/in_maces.c
@@ -1700,6 +1700,39 @@ static int in_maces_init(struct flb_input_instance *ins, struct flb_config *conf
                 encode_audit_token_t(encoder, &od_create_user->instigator_token);
                 flb_log_event_encoder_body_commit_map(encoder);
                 break;
+            case ES_EVENT_TYPE_NOTIFY_OD_CREATE_GROUP:
+                es_event_od_create_group_t *od_create_group = event.od_create_group;
+                flb_log_event_encoder_body_begin_map(encoder);
+                if (od_create_group->instigator) {
+                  flb_log_event_encoder_append_body_cstring(
+                      encoder,
+                      "instigator");
+                  encode_es_process_t(encoder, od_create_group->instigator);
+                }
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("error_code"),
+                    FLB_LOG_EVENT_INT32_VALUE(od_create_group->error_code));
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("group_name"),
+                    FLB_LOG_EVENT_STRING_VALUE(od_create_group->group_name.data, od_create_group->group_name.length));
+                flb_log_event_encoder_append_body_values(
+                    encoder,
+                    FLB_LOG_EVENT_CSTRING_VALUE("node_name"),
+                    FLB_LOG_EVENT_STRING_VALUE(od_create_group->node_name.data, od_create_group->node_name.length));
+                if(od_create_group->db_path.length > 0) {
+                    flb_log_event_encoder_append_body_values(
+                        encoder,
+                        FLB_LOG_EVENT_CSTRING_VALUE("db_path"),
+                        FLB_LOG_EVENT_STRING_VALUE(od_create_group->db_path.data, od_create_group->db_path.length));
+                }
+                flb_log_event_encoder_append_body_cstring(
+                    encoder,
+                    "instigator_token");
+                encode_audit_token_t(encoder, &od_create_group->instigator_token);
+                flb_log_event_encoder_body_commit_map(encoder);
+                break;
             default:
                 flb_log_event_encoder_append_body_null(encoder);
                 break;
